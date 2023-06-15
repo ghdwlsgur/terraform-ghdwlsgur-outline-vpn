4a59ecec46d582b4554f179a4d1908d40e9753d0
ghdwlsgur <vjinhyeokv@gmail.com>
2023-06-15 14:21:33 +0900

    delete ec2 ssm role

--- role.tf
+++ role.tf
@@ -1,24 +1,24 @@
-resource "aws_iam_role" "ec2_ssm_role" {
-  name = "ec2_ssm_role-${var.aws_region}"
+# resource "aws_iam_role" "ec2_ssm_role" {
+#   name = "ec2_ssm_role-${var.aws_region}"
 
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : {
-      "Effect" : "Allow",
-      "Principal" : {
-        "Service" : "ec2.amazonaws.com"
-      },
-      "Action" : "sts:AssumeRole"
-    }
-  })
-}
+#   assume_role_policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : {
+#       "Effect" : "Allow",
+#       "Principal" : {
+#         "Service" : "ec2.amazonaws.com"
+#       },
+#       "Action" : "sts:AssumeRole"
+#     }
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "ssm_role_attach" {
-  role       = aws_iam_role.ec2_ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
-}
+# resource "aws_iam_role_policy_attachment" "ssm_role_attach" {
+#   role       = aws_iam_role.ec2_ssm_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+# }
 
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile-${var.aws_region}"
-  role = aws_iam_role.ec2_ssm_role.name
-}
+# resource "aws_iam_instance_profile" "ec2_profile" {
+#   name = "ec2_profile-${var.aws_region}"
+#   role = aws_iam_role.ec2_ssm_role.name
+# }
