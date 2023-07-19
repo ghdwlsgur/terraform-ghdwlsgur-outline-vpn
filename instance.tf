b29d2f8646daf441a331f7d8f69b9404ef1c41a1
HongJinHyeok <vjinhyeokv@gmail.com>
2023-07-19 14:14:08 +0900

    edit env

--- instance.tf
+++ instance.tf
@@ -60,11 +60,10 @@ resource "terraform_data" "create_securitygroup_rules" {
   ]
 }
 
-
 data "external" "outline_vpn_local_path" {
   program = <<EOT
     outline_vpn_path=$(which outline-vpn)
-    lib_path=\${dirname(outline_vpn_path)}/lib
+    lib_path=\${dirname("$outline_vpn_path")}/lib
     echo "{ \"path\": \"\$lib_path\" }"
   EOT
 }
